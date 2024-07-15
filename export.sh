c8e16f7f4f6db6e87f104cd39ef9af6bc9cdb691
David C <20852399+davidwashere@users.noreply.github.com>
2024-07-14 23:40:53 -0500

    enhance export

--- export.sh
+++ export.sh
@@ -4,3 +4,5 @@ rm -rf ./builds/web || true
 mkdir -p ./builds/web
 cp enable-threads.js ./builds/web/
 godot --headless --export-release Web ./builds/web/index.html
+
+cp -r ./builds/web/* ./pages-worktree
