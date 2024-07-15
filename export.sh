2c5c346ce8933022d98883ba69dcf9df14fdd4d2
David C <20852399+davidwashere@users.noreply.github.com>
2024-07-14 23:48:55 -0500

    remove audio

--- export.sh
+++ export.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+set -e
+
 rm -rf ./builds/web || true
 mkdir -p ./builds/web
 cp enable-threads.js ./builds/web/
 godot --headless --export-release Web ./builds/web/index.html
 
 cp -r ./builds/web/* ./pages-worktree
+cd ./pages-worktree
+git add . && git commit -m "export" && git push
