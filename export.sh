c6f98f74ee61b85aba0d48d1454c535a77a70fc2
Dave <20852399+davidwashere@users.noreply.github.com>
2025-10-29 12:05:56 -0500

    inc version, update to run on mac

--- export.sh
+++ export.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+GODOT=/Applications/Godot.app/Contents/MacOS/Godot
+
 set -e
 
 rm -rf ./builds/web || true
 mkdir -p ./builds/web
 cp enable-threads.js ./builds/web/
-godot --headless --export-release Web ./builds/web/index.html
+$GODOT --headless --export-release Web ./builds/web/index.html
 
 cp -r ./builds/web/* ./pages-worktree
 cd ./pages-worktree
